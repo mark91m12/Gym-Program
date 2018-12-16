3186db875fca12b5841f8717da0ea8e1810be6c3
mark91m12 <amatomarco1@gmail.com>
2018-12-16 19:55:27 +0100

    Competitor

---
+++ Gym-Program/src/com/gym/program/logic/Competitor.java
@@ -0,0 +1,17 @@
+package com.gym.program.logic;
+
+enum Category {
+	
+}
+
+enum Sex {
+	MALE, FEMALE
+}
+
+public class Competitor {
+
+	private String name;
+	private String surname;
+	private double weight;
+
+}
