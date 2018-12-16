985275a21f83d3d5ef5b199188e07b796f099d81
mark91m12 <amatomarco1@gmail.com>
2018-12-16 20:23:06 +0100

    added categories for competitor

--- Gym-Program/src/com/gym/program/logic/Competitor.java
+++ Gym-Program/src/com/gym/program/logic/Competitor.java
@@ -1,7 +1,12 @@
 package com.gym.program.logic;
 
-enum Category {
-	
+enum Age {
+	SUB_JUNIOR, JUNIOR, OPEN, MASTER1, MASTER2, MASTER3, MASTER4
+}
+
+enum Weight {
+	MINUS_43,MINUS_47, MINUS_52, MINUS_57, MINUS_63, MINUS_72, MINUS_84, PLUS_84,
+	MINUS_53,MINUS_59, MINUS_66, MINUS_74, MINUS_83, MINUS_93, MINUS_105, MINUS_120, PLUS_120,
 }
 
 enum Sex {
